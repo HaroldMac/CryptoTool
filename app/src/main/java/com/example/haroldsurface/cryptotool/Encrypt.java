53d1667ba514b074930c3d4516b0ddcfd91f6c6a
Harold MacDonald <harold.macdonald52@gmail.com>
2017-09-26 22:33:27 -0600

    Added initial shift cipher. No error correction on inputs

--- app/src/main/java/com/example/haroldsurface/cryptotool/Encrypt.java
+++ app/src/main/java/com/example/haroldsurface/cryptotool/Encrypt.java
@@ -1,20 +1,21 @@
 package com.example.haroldsurface.cryptotool;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class Encrypt extends AppCompatActivity {
 
-    EditText plainText;
+    EditText plainText, keyText;
     TextView cipherText;
+    Button encryptButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +25,18 @@ public class Encrypt extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         plainText = (EditText) findViewById(R.id.plainTxt);
+        keyText = (EditText) findViewById(R.id.keyTxt);
         cipherText = (TextView) findViewById(R.id.cipcherTxt);
+        encryptButton = (Button) findViewById(R.id.encrytpBtn);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        encryptButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String message = plainText.getText().toString();
-                cipherText.setText(message);
+                int key =  Integer.parseInt(keyText.getText().toString());
+                cipherText.setText(shiftMessage(message, key));
 
-                Snackbar.make(view, "Replace with your own sexy action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
             }
         });
     }
@@ -59,5 +61,24 @@ public class Encrypt extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private String shiftMessage(String message, int key){
+        String cipher = "";
+        message = message.toUpperCase();
+        for (int i=0; i< message.length(); i++){
+            char aChar = (message.charAt(i));
+            if ((aChar > 'Z') || (aChar < 'A')){
+                cipher += aChar;
+                continue;
+            }
+            aChar = (char)(aChar + (key % 26));
+            if (aChar > 'Z'){
+                cipher += (char)(message.charAt(i) - (26 - (key % 26)) );
+            } else {
+                cipher += (char)(message.charAt(i) + (key % 26));
+            }
+        }
+        return cipher;
     }
 }
